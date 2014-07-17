145e3e44ffdf3cc097b9c9367a2a0d1e56235ccf
domsmasher <matteo.gildone@gmail.com>
2014-07-17 07:09:12 +0100

    added bourbon, js structure

--- model/test.js
+++ model/test.js
@@ -1,3 +1,22 @@
-/**
- * Created by domsmasher on 05/05/2014.
- */
+var nano = require('nano')('http://localhost:5984'),
+    test = nano.db.use('myfirsttest');
+
+exports.test = function (req, res) {
+    // clean up the database we created previously
+    nano.db.destroy('alice', function() {
+        // create a new database
+        nano.db.create('alice', function() {
+            // specify the database we are going to use
+            var alice = nano.use('alice');
+            // and insert a document in it
+            alice.insert({ crazy: true }, 'rabbit', function(err, body, header) {
+                if (err) {
+                    console.log('[alice.insert] ', err.message);
+                    return;
+                }
+                console.log('you have inserted the rabbit.')
+                console.log(body);
+            });
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,3 +1,39 @@
 /**
- * Created by domsmasher on 27/04/2014.
+ * Module dependencies.
  */
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    test = require('./model/test');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Home', activeMenu: 'home' });
+});
+
+app.get('/test/', test.test);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
